5afa16b96305ec68187fa5badb2b6e9b2e1b54b7
AvinashMahesh <87287679+AvinashMahesh@users.noreply.github.com>
2021-10-14 06:02:03 -0400

    Update Statistics.java

---
+++ StatisticsTester.java
@@ -0,0 +1,24 @@
+public class StatisticsTester {
+
+	public static void main(String[] args) {
+		
+		Statistics arry = new Statistics();
+		
+		double avg = arry.calcAvg();
+		int min = arry.getMin();
+		int max = arry.getMax();
+		double dev = arry.calcStdDev();
+		
+		System.out.println("Array Average: " + avg);
+		System.out.println("Array Min: " + min);
+		System.out.println("Array Max: " + max);
+		System.out.println("Standard Deviation: " + dev); 
+		
+		arry.displayHistogram();
+		
+		System.out.println();
+		String a = arry.toString();
+		System.out.println(a);
+	}
+
+}
