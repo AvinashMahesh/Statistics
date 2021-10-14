eba442e10651ee0e2aeb4ad2a2dafb7ef813d783
AvinashMahesh <87287679+AvinashMahesh@users.noreply.github.com>
2021-10-14 06:00:59 -0400

    Update Statistics.java

--- Statistics.java
+++ Statistics.java
@@ -67,4 +67,61 @@ public class Statistics {
 	}  
 	  //returns the standard deviation using values from the array and calculated
 	  //from the following formula
+	  public void displayHistogram() { 
+		int [] freq = new int[18];
+
+		for(int b = 0; b < freq.length; b++) {
+			freq[b] = 0;
+		}
+
+		int n = 0;
+
+		for(int i = 0; i < array.length; i++) {
+			n = array[i];
+			freq[n - 1]++;
+		}
+
+		int times = 0;
+		int num = 1;
+
+		System.out.println("  Frequency Distribution Table");
+
+		System.out.print("  ----------------------------\n\n");
+
+		for(int a = 0; a < freq.length; a++) {
+			times = freq[a] / 5;
+			
+			if(freq[a] % 5 != 0)
+				times ++;
+			
+			System.out.print(num + " | \t");
+
+			for(int c = 0; c < times; c++) {
+				System.out.print("*");
+			}
+
+			System.out.println();
+			num++;
+		}
+		
+	}  
+	   
+
+	public String toString() { 
+		String grid = "";
+		int count = 0;
+		for(int a = 0; a < array.length; a++) {
+			grid += array[a] + "   ";
+			count ++;
+			
+			if(count % 25 == 0) {
+				grid += "\n";
+			}
+		}
+		return grid; 
+	}  
+
+	
+}
+
 
